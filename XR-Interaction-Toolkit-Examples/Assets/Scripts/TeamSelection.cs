e56e91942f1381f3aa22a5f998ccbce18c53c6e7
Sean DeGrazia <146482091+seand1012@users.noreply.github.com>
2024-04-21 19:07:39 -0500

    Minor modification to team selection

--- XR-Interaction-Toolkit-Examples/Assets/Scripts/TeamSelection.cs
+++ XR-Interaction-Toolkit-Examples/Assets/Scripts/TeamSelection.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TeamSelection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ChooseHeroes()
     {
-        
+        StartLightCycleBattle("Heroes");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChooseVillains()
     {
-        
+        StartLightCycleBattle("Villains");
     }
+
+    private void StartLightCycleBattle(string team)
+    {
+        SceneManager.LoadScene("TronLevel", LoadSceneMode.Single);
+        // Pass chosen team to LightCycleBattle scene using PlayerPrefs or other methods
+        PlayerPrefs.SetString("ChosenTeam", team);
+    }
+
+    
 }
