3203731240267dff816f408c959bfdf3c050297f
Sean DeGrazia <146482091+seand1012@users.noreply.github.com>
2024-04-25 15:20:37 -0500

    Multiplayer work

--- XR-Interaction-Toolkit-Examples/Assets/Scripts/TeamSelection.cs
+++ XR-Interaction-Toolkit-Examples/Assets/Scripts/TeamSelection.cs
@@ -5,21 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class TeamSelection : MonoBehaviour
 {
+    int counter = 0;
     public void ChooseHeroes()
     {
+        counter++;
         StartLightCycleBattle("Heroes");
     }
 
     public void ChooseVillains()
     {
+        counter++;
         StartLightCycleBattle("Villains");
+        
     }
 
     private void StartLightCycleBattle(string team)
     {
-        SceneManager.LoadScene("TronLevel", LoadSceneMode.Single);
-        // Pass chosen team to LightCycleBattle scene using PlayerPrefs or other methods
-        PlayerPrefs.SetString("ChosenTeam", team);
+        
+        if(counter >= 2)
+        {
+            SceneManager.LoadScene("TronLevel", LoadSceneMode.Single);
+            // Pass chosen team to LightCycleBattle scene using PlayerPrefs or other methods
+            PlayerPrefs.SetString("ChosenTeam", team);
+        }
+        else
+        {
+            SceneManager.LoadScene("Station_01_Setup_Isolated", LoadSceneMode.Single);
+        }
+        
     }
 
     
