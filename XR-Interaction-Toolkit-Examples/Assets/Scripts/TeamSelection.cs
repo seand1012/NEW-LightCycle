aab485fa5f3e554055c92d43bf8cfe5a03529669
Sean DeGrazia <146482091+seand1012@users.noreply.github.com>
2024-04-28 21:52:59 -0500

    Multiplayer changes

--- XR-Interaction-Toolkit-Examples/Assets/Scripts/TeamSelection.cs
+++ XR-Interaction-Toolkit-Examples/Assets/Scripts/TeamSelection.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Unity.Netcode;
 
-public class TeamSelection : MonoBehaviour
+public class TeamSelection : NetworkBehaviour
 {
+    [SerializeField] public NetworkObject player;
+    [SerializeField] public Camera startingCamera;
     public NetworkManager networkManager;
     public void ChooseTeam(string team)
     {
@@ -14,17 +16,21 @@ public class TeamSelection : MonoBehaviour
         {
             // Set the chosen team
             PlayerPrefs.SetString("ChosenTeam", team);
-
+            
+            networkManager.StartHost();
             // Start the multiplayer scene
-            SceneManager.LoadScene("Assets/Scenes/TronLevel.unity", LoadSceneMode.Single);
+            //SceneManager.LoadScene("Assets/Scenes/TronLevel.unity", LoadSceneMode.Single);
         }
         else
         {
+            networkManager.StartClient();
             // Start the scene for subsequent players (not host)
             //SceneManager.LoadScene("Assets/Scenes/TronLevel.unity", LoadSceneMode.Single);
-            networkManager.StartClient();
+            
         }
 
-
+        startingCamera.depth = -2;
     }
+
+
 }
