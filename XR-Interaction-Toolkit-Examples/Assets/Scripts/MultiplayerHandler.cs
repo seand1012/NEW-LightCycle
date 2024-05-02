2241dc1ccdefd2f81442aae7a95449bb699fc533
Sean DeGrazia <146482091+seand1012@users.noreply.github.com>
2024-05-01 20:11:51 -0500

    Commented line out

--- XR-Interaction-Toolkit-Examples/Assets/Scripts/MultiplayerHandler.cs
+++ XR-Interaction-Toolkit-Examples/Assets/Scripts/MultiplayerHandler.cs
@@ -6,6 +6,7 @@ using Unity.Netcode;
 using UnityEngine.SceneManagement;
 using Unity.Networking.Transport;
 using Unity.XR.CoreUtils;
+using Unity.Netcode.Transports.UTP;
 
 public class MultiplayerHandler : NetworkBehaviour
 {
@@ -21,6 +22,7 @@ public class MultiplayerHandler : NetworkBehaviour
 
     void Start()
     {
+        //networkManager.transform = NetworkManager.Singleton.gameObject.AddComponent<UNETTransport>();
         heroesButton.onClick.AddListener(() => ChooseTeam("Heroes"));
         villainsButton.onClick.AddListener(() => ChooseTeam("Villains"));
     }
